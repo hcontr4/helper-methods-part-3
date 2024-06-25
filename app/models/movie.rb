25179783ede0155f02d20e5bf7810465e5124e91
Herman Contreras <contrerasherman2@gmail.com>
2024-06-25 16:26:24 -0500

    Params helper method implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,10 +4,11 @@
 #
 #  id          :bigint           not null, primary key
 #  description :text
+#  image_url   :string
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
 class Movie < ApplicationRecord
-  validates :title, presence: true
+  validates :title, :image_url, presence: true
 end
